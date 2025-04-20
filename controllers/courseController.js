b29f660fd0567f7e04a20c886fb61ec432aef2d1
RhythmicRhythm <okubadejorh@gmail.com>
2025-04-20 19:50:52 +0100

    Refactor Firebase setup to use environment variables for configuration

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -11,14 +11,25 @@ cloudinary.config({
   secure: true,
 });
 
-//Firebase setup
-const serviceAccount = require("../edu-tech-rhythmic-firebase-adminsdk-fn22u-086ee594f1.json");
+// Firebase setup
+const firebaseConfig = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseConfig),
   storageBucket: "gs://edu-tech-rhythmic.appspot.com",
 });
-
-// const bucket = admin.storage().bucket();
 
 const createCourse = asyncHandler(async (req, res) => {
   try {
